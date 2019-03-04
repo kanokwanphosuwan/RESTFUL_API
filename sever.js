415f21625cbd2affc7a45d8f62b650550cd803c9
kanokwanphosuwan <kanokwanphosuwan.com>
2019-03-04 21:00:14 +0700

    add new product

--- sever.js
+++ sever.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var bodyPrser = require('body-parser');
 var app = express();
-var port = process.env.PORT || 8081;
+var port = process.env.PORT || 3000;
 
 
-const books = require('./analytic-trash-export');
+var books = require('./analytic-trash-export');
 
 app.get('/data', (req, res) =>
 {
-    res.json(books);
+    res.send(books);
     
 });
+
+
 
 app.post('/data', (req, res)=>
 {
